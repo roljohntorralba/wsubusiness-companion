68f10d9d818dba205bb4409438f7f2cc733fd71b
Rol John Torralba <roljohntorralba@gmail.com>
2019-10-21 17:12:59 +0800

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== WebsiteSetup Business Companion ===
+Contributors: websitesetup
+Tags: gutenberg, theme extension
+Requires at least: 5.0
+Tested up to: 5.2
+Requires PHP: 7.2
+Stable tag: 1.0.0
+License: GNU General Public License v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Extends the WebsiteSetup Business theme\'s functionality. Adding the ability to create lead texts and sidebar content per page.
+
+== Description ==
+The WebsiteSetup Business Companion plugin extends the WebsiteSetup Business theme. Adding a number of features to the admin panel that enhances the theme\'s customizability.
+
+* Lead Text
+With the Lead Text feature, authors can now add a short text below the title on their posts or pages.
+
+* Sidebar Content
+Authors can add content to their posts or pages\' sidebar that appears on top of the widgets. This gives the ability to create unique \"text widget-like\" content that is unique for every post or page.
+
+* Custom Gutenberg Blocks
+Gutenberg blocks specially made for the WebsiteSetup Business theme.
+
+== Installation ==
+1. In your admin panel, go to Plugins and click the Add New button.
+2. Click Upload Plugin and Choose File, then select the plugin\'s .zip file. Click Install Now.
+3. Click Activate to use your new plugin right away.
+
+== Frequently Asked Questions ==
+= Can I use this plugin with other themes? =
+
+No. Installing this plugin with other themes will give you unexpected results since the plugin targets WebsiteSetup Business theme-specific functions only.
+
+= Is this plugin required by WebsiteSetup Business theme? =
+
+No, it is not required but it is recommended to maximize the theme\'s functionality and editing experience.
+
+== Screenshots ==
+1. Screenshot 1 desc
+2. Screenshot 2 desc
+3. Screenshot 3 desc
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Credits ==
+* Meta Box https://metabox.io, (C) 2019 elightup [GPLv2 or later](https://www.gnu.org/licenses/gpl-2.0.html)
---
+++ wsubusiness-companion.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Plugin Name: WebsiteSetup Business Companion
+ * Plugin URI: https://wordpress.org/plugins/wsubusiness-companion/
+ * Description: Extends the WebsiteSetup Business theme\&#39;s functionality. Adding the ability to create lead texts and sidebar content per page.
+ * Version: 1.0.0
+ * Requires at least: 5.2
+ * Requires PHP: 7.2
+ * Author: WebsiteSetup
+ * Author URI: https://websitesetup.org/about-and-contact/
+ * Text Domain: wsubusiness-companion
+ * Domain Path: /languages
+ * License: GPL2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ 
+ WebsiteSetup Business Companion is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
+ 
+ WebsiteSetup Business Companion is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along with WebsiteSetup Business Companion. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
+
+ */
+
+//* Block direct access to the file
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+//* Prints the Leading Text custom field below the title
+function add_hero_text() {
+	$lead_text = get_post_meta( get_the_id(), '_wsubc_lead_text_value', true);
+    if( $lead_text ) {
+        echo '<p class="lead">' . $lead_text . '</p>';
+    }
+}
+add_action( 'in_entry_header_bottom', 'add_hero_text');
+
+//* Prints the Sidebar Text custom field on top of the sidebar widgets
+function wsubc_render_sidebar_text() {
+	$sidebar_text = get_post_meta( get_the_id(), '_wsubc_sidebar_text_value', true);
+	$output = '<section id="custom-sidebar-text" class="widget widget-custom-sidebar-text">';
+	$output .= $sidebar_text;
+	$output .= '</section>';
+	echo $output;
+}
+add_action( 'in_sidebar_top', 'wsubc_render_sidebar_text');
+
+//* Leading text 
+abstract class WSUBC_Lead_Text {
+    public static function add() {
+        $screens = ['post', 'page'];
+        foreach ($screens as $screen) {
+            add_meta_box(
+                'wsubc_lead_text_box',          // Unique ID
+                __('Custom fields', 'wsubusiness-companion'), // Box title
+                [self::class, 'lead_text_html'],   // Content callback, must be of type callable
+                $screen,                  // Post type
+                'side'
+            );
+            add_meta_box(
+                'wsubc_sidebar_text_box',          // Unique ID
+                __('Sidebar text', 'wsubusiness-companion'), // Box title
+                [self::class, 'sidebar_text_html'],   // Content callback, must be of type callable
+                $screen,                  // Post type
+                'side'
+            );
+        }
+    }
+ 
+    public static function lead_text_save($post_id) {
+        if (array_key_exists('wsubc_lead_text_field', $_POST)) {
+            update_post_meta(
+                $post_id,
+                '_wsubc_lead_text_value',
+                sanitize_text_field($_POST['wsubc_lead_text_field'])
+            );
+        }
+    }
+ 
+    public static function lead_text_html($post) {
+    	wp_nonce_field( basename( __FILE__ ), 'wsubc_nonce' );
+	    $wsubc_stored_meta = get_post_meta( $post->ID );
+	    ?>
+	    <p>
+        <label for="wsubc_lead_text_field" class="components-text-control__label"><?php _e( 'Leading text', 'wsubusiness-companion' )?></label>
+        <input type="text" class="components-text-control__input" name="wsubc_lead_text_field" id="wsubc_lead_text_field" value="<?php if ( isset ( $wsubc_stored_meta['_wsubc_lead_text_value'] ) ) echo $wsubc_stored_meta['_wsubc_lead_text_value'][0]; ?>" />
+	    </p>
+	    <?php
+    }
+
+    public static function sidebar_text_save($post_id) {
+        if (array_key_exists('wsubc_sidebar_text_field', $_POST)) {
+        	if( !empty($_POST['wsubc_sidebar_editor']) ) {
+        		$data = $_POST['wsubc_sidebar_editor'];
+        		update_post_meta(
+              $post_id,
+              '_wsubc_sidebar_text_value',
+              $data
+            );
+        	}
+        }
+    }
+
+    public static function sidebar_text_html($post) {
+    	wp_nonce_field( basename( __FILE__ ), 'wsubc_nonce' );
+	    $wsubc_stored_meta = get_post_meta( $post->ID );
+	    wp_editor($wsubc_stored_meta['_wsubc_sidebar_text_value'][0], 'wsubc_sidebar_editor', array( "media_buttons" => true));
+	    ?>
+	    <p>
+        <label for="wsubc_sidebar_text_field" class="components-text-control__label"><?php _e( 'Sidebar text', 'wsubusiness-companion' )?></label>
+        <textarea name="wsubc_sidebar_text_field" id="wsubc_sidebar_text_field"><?php if ( isset ( $wsubc_stored_meta['_wsubc_sidebar_text_value'] ) ) echo $wsubc_stored_meta['_wsubc_sidebar_text_value'][0]; ?></textarea>
+	    </p>
+	    <?php
+    }
+}
+add_action('add_meta_boxes', ['WSUBC_Lead_Text', 'add']);
+add_action('save_post', ['WSUBC_Lead_Text', 'lead_text_save']);
+add_action('save_post', ['WSUBC_Lead_Text', 'sidebar_text_save']);
+
+?>
